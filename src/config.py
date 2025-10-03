624bf086f62b1519fa77447ad02a2c969672f189
Emmanuel LUSAMBULU LUA NZAMBI <@emmanuel.lusambululuanzambi>
2025-10-03 15:24:47 +0200

    [MODIF] fix name of db

--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017/greenstyle")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017/greenstyle_DB")
