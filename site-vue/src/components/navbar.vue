143c107ddc976c9e97f784a7facad5e21a991488
Solaine Doumbe <doumbe_f@etna-alternance.net>
2025-12-17 16:41:06 +0100

    Ajout page À propos de nous et refonte navigation

--- site-vue/src/components/navbar.vue
+++ site-vue/src/components/navbar.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { RouterLink } from "vue-router";
 const props = defineProps({
-  logoSrc: { type: String, default: '/src/assets/heel-plant.png' },
-})
+  logoSrc: { type: String, default: "/src/assets/heel-plant.png" },
+});
 </script>
 
 <template>
@@ -15,7 +15,10 @@ const props = defineProps({
       <nav aria-label="Navigation principale">
         <a class="link left-1" href="#">Recherche détaillée</a>
         <a class="link left-2" href="#">Tableau de bord</a>
-        <a class="link right-1" href="#">À propos de nous</a>
+
+        <RouterLink class="link right-1" to="/about">
+          À propos de nous
+        </RouterLink>
         <a class="link right-2" href="#">Mon compte</a>
       </nav>
 
@@ -25,38 +28,42 @@ const props = defineProps({
 </template>
 
 <style scoped>
-.nav-root{
+.nav-root {
   background: var(--page-bg);
-  position: sticky; top:0; left:0; right:0; z-index:1000;
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
 }
 
-.nav-inner{
+.nav-inner {
   width: 100%;
   height: 85px;
   position: relative;
 }
 
-.nav-rule{
+.nav-rule {
   position: absolute;
   left: 5%;
   top: 80px;
   width: 90vw;
   height: 1px;
   background: #fff;
-  transform: scaleY(.5);
+  transform: scaleY(0.5);
   transform-origin: top left;
   z-index: 2;
   pointer-events: none;
 }
 
-.logo{
+.logo {
   position: absolute;
   top: 8px;
   width: 61px;
   height: auto;
 }
 
-.link{
+.link {
   position: absolute;
   top: 34px;
   font-family: "Jersey 10", system-ui, sans-serif;
@@ -67,11 +74,24 @@ const props = defineProps({
   text-decoration: none;
   text-shadow: 5px 5px 8px #ffffff;
 }
-.link:hover{ text-decoration: underline; }
-.link:focus-visible{ outline: 2px solid var(--green); outline-offset: 2px; }
+.link:hover {
+  text-decoration: underline;
+}
+.link:focus-visible {
+  outline: 2px solid var(--green);
+  outline-offset: 2px;
+}
 
-.left-1 { left: 4.5%; }
-.left-2 { left: 14.5%; }
-.right-1{ right: 15%; }
-.right-2{ right: 5%; }
+.left-1 {
+  left: 4.5%;
+}
+.left-2 {
+  left: 14.5%;
+}
+.right-1 {
+  right: 15%;
+}
+.right-2 {
+  right: 5%;
+}
 </style>
